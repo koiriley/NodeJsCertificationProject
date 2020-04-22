4424ef1f9896e960636c3c503cf30829a8a64d51
Shaheda Choudhury <54161228+shahedakc@users.noreply.github.com>
2020-04-22 15:06:39 -0400

    update - frontend working home page

--- application.js
+++ application.js
@@ -9,18 +9,19 @@ app.use("/", HomePageRouter)
 const AboutPageRouter = require('./routes/about')
 app.use("/about", AboutPageRouter)
 
-const ContactPageRouter = require('./routes/contact')
+/* const ContactPageRouter = require('./routes/contact')
 app.use("/contact", ContactPageRouter)
-
 
 const SportsPageRouter = require('./routes/sports')
 app.use("/sports", SportsPageRouter)
+*/
 
 app.use(express.static(path.join(__dirname, "public")))
 
 app.set("view engine", "ejs")
-app.set('views','./views')
+app.set('views', './views')
 
+//app.get('/about', (req,res)=>{res.render('about')})
 
 
 module.exports = app;
